77575a50f019d8f12187daeae64705757bb636f9
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-12-11 14:32:50 +0100

    Add configuration for webpack and babel and pahser

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,28 @@
 const path = require('path');
+
+module.exports ={
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+  }
+}
