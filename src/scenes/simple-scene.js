e9742825abb13e169d3b72db94c33e158220ab1b
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-12-12 13:47:11 +0100

    load image assets

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -1,5 +1,9 @@
 export class SimpleScene extends Phaser.Scene {
+  preload(){
+    this.load.image('background', '../assets/background.png');
+  }
   create() {
+    this.add.image(100,200,'background')
     this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin =  require('copy-webpack-plugin')
 const webpack = require('webpack');
 
 module.exports ={
