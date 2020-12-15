2aaae3d3c52eca2d961163cad9171c4fb7997f0d
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-12-15 09:51:37 +0100

    Add platform to game

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,4 +1,5 @@
 import 'phaser';
+let gameState ={}
  
 export default class GameScene extends Phaser.Scene {
   constructor () {
@@ -8,15 +9,17 @@ export default class GameScene extends Phaser.Scene {
   preload () {
     // load images
     this.load.image('bg', 'assets/background.png');
-    this.load.image('platform', '../assets/platform.png');
+    this.load.image('platform1', '../assets/platform.png');
+    this.load.image('platform2', '../assets/platform.png');
+
   }
  
   create () {
-    this.add.image(500, 300, 'bg').setScale(2)
-   gameState.platform = this.add.image(250, 550, 'platform')
+    this.add.image(500, 300, 'bg').setScale(2);
 
-    // this.add.sprite(200, 200, 'hero-spine')
-    // this.add.text(100,200, "Hello phaser!")
+    const platforms = this.physics.add.staticGroup();
+    platforms.create(250, 550, 'platform1').refreshBody();
+    platforms.create(600, 550, 'platform2').refreshBody();
   }
 
 };
